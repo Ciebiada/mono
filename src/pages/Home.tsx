9c7f8b68d6f31e22efc2f2add56f0aceef01e5c0
Michał Ciebiada <ciebiada@gmail.com>
2025-06-22 14:17:49 +0200

    The editor seems to work nice on both ios and desktop

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,19 +12,88 @@ import {
   IonList,
   IonModal,
   IonPage,
-  IonRouterLink,
   IonSearchbar,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
 import "./Home.css";
 import { search } from "ionicons/icons";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
+import { StarterKit } from "@tiptap/starter-kit";
+import { Placeholder } from "@tiptap/extensions";
+import { EditorContent, useEditor } from "@tiptap/react";
+
+const extensions = [
+  StarterKit,
+  Placeholder.configure({ placeholder: "Write your note here..." }),
+];
 
 const Home: React.FC = () => {
+  const editor = useEditor({
+    extensions,
+    // editorProps: {
+    //   scrollThreshold: 500,
+    //   scrollMargin: 500,
+    // },
+    onSelectionUpdate: ({ editor }) => {
+      const { selection } = editor.state;
+
+      const cursor = editor.view.coordsAtPos(selection.from);
+
+      setCursorTop(cursor.bottom);
+
+      if (contentRef.current && window.visualViewport) {
+        // if (cursor.bottom > 400) {
+          // contentRef.current?.scrollByPoint(0, 100, 100);
+        // }
+
+          const visualViewportHeight = window.visualViewport.height;
+
+          const margin = visualViewportHeight * 0.2;
+          if (cursor.top > visualViewportHeight - margin) {
+            const scrollAmount = cursor.top - (visualViewportHeight - margin);
+
+            contentRef.current?.scrollByPoint(0, scrollAmount, 100);
+          }
+        }
+      }
+    })
+
   const modal = useRef<HTMLIonModalElement>(null);
   const searchbar = useRef<HTMLIonSearchbarElement>(null);
+  const contentRef = useRef<HTMLIonContentElement>(null);
+  const [ viewportOffset, setViewportOffset ] = useState(0);
+  const [ isKeyboardOpen, setIsKeyboardOpen ] = useState(false);
+  const [ keyboardHeight, setKeyboardHeight ] = useState(0);
+  const [ cursorTop, setCursorTop ] = useState(0);
+
+  useEffect(() => {
+    editor?.commands.focus();
+  }, [editor]);
+
+  useEffect(() => {
+    const handleViewportChange = () => {
+      if (window.visualViewport) {
+        setViewportOffset(window.visualViewport.height + window.visualViewport.offsetTop);
+        setIsKeyboardOpen(window.visualViewport.height / window.innerHeight < 0.75);
+        setKeyboardHeight(window.innerHeight - window.visualViewport.height);
+       }
+    };
+
+    if (window.visualViewport) {
+      window.visualViewport.addEventListener('resize', handleViewportChange);
+      window.visualViewport?.addEventListener('scroll', handleViewportChange);
+      window.addEventListener('touchmove', handleViewportChange);
+
+      handleViewportChange();
+
+      return () => {
+        window.visualViewport?.removeEventListener('resize', handleViewportChange);
+        window.visualViewport?.removeEventListener('scroll', handleViewportChange);
+        window.removeEventListener('touchmove', handleViewportChange);
+      };
+    }
+  }, []);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -33,22 +102,39 @@ const Home: React.FC = () => {
         modal.current?.present();
       }
     };
-
     document.addEventListener("keydown", handleKeyDown);
-    return () => document.removeEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
   }, []);
 
   return (
     <IonPage>
-      <IonContent fullscreen>
+      {/* <div style={{ position: 'absolute', zIndex: 100, top: viewportOffset - 40, width: '100%', height: 40, backgroundColor: 'red' }}>
+        {window?.visualViewport?.height} {window?.innerHeight}
+      </div> */}
+      <IonHeader translucent>
+        <IonToolbar>
+          <IonTitle>Untitled</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent ref={contentRef} fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Untitled</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <div
+          className="ion-padding"
+          style={{
+            '--tiptap-padding-bottom': `${keyboardHeight + 100}px`
+          } as React.CSSProperties}
+        >
+          <EditorContent editor={editor} />
+        </div>
       </IonContent>
-      <IonFooter>
+      <IonFooter translucent>
         <IonToolbar>
           <div style={{ display: "flex", justifyContent: "center" }}>
             <IonButtons slot="primary">
@@ -58,11 +144,14 @@ const Home: React.FC = () => {
               <IonModal
                 ref={modal}
                 trigger="open-modal"
+                onDidDismiss={() => {
+                  editor?.commands.focus();
+                }}
                 onDidPresent={() => {
                   searchbar.current?.setFocus();
                 }}
-                initialBreakpoint={0.25}
-                breakpoints={[0, 0.25, 0.5, 0.75]}
+                initialBreakpoint={0.3}
+                breakpoints={[0.3, 0.6]}
               >
                 <IonContent className="ion-padding">
                   <IonSearchbar
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+    })
   ],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
   }
 })
